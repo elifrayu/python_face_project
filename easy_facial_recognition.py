9bc7a8b07008a487a2b87a5c69fbdbbd92666810
elif uyar <elif.uyar.t3@gmail.com>
2025-04-07 19:31:34 +0300

    şimdi yaptım 1
    ekeke

--- easy_facial_recognition.py
+++ easy_facial_recognition.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
 
     print('[INFO] Faces well imported')
     print('[INFO] Starting Webcam...')
-    video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(1)
     print('[INFO] Webcam well started')
     print('[INFO] Detecting...')
     while True:
@@ -121,5 +121,7 @@ if __name__ == '__main__':
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     print('[INFO] Stopping System')
+
+
     video_capture.release()
     cv2.destroyAllWindows()
